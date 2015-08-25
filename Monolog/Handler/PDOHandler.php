639eb72d75b4d2bab3d8f3d521b583f10cea9541
Monnot Stéphane <monnot.stephane@gmail.com>
2015-08-25 16:11:05 +0200

    Ajout de l'ip du client dans le PDOHandler de Monolog

--- Monolog/Handler/PDOHandler.php
+++ Monolog/Handler/PDOHandler.php
@@ -4,6 +4,7 @@ namespace Solire\Lib\Monolog\Handler;
 
 use Monolog\Logger;
 use Monolog\Handler\AbstractProcessingHandler;
+use Solire\Lib\Registry;
 
 /**
  * Classe PDO Handler pour Monolog
@@ -43,11 +44,14 @@ class PDOHandler extends AbstractProcessingHandler
             $this->initialize();
         }
 
+        $remoteIp = Registry::get('request')->getClientIp();
+
         $this->statement->execute([
-            'channel' => $record['channel'],
-            'level'   => $record['level'],
-            'message' => $record['formatted'],
-            'time'    => $record['datetime']->format('U'),
+            'channel'  => $record['channel'],
+            'level'    => $record['level'],
+            'message'  => $record['formatted'],
+            'time'     => $record['datetime']->format('U'),
+            'remoteip' => $remoteIp
         ]);
     }
 
@@ -60,10 +64,10 @@ class PDOHandler extends AbstractProcessingHandler
     {
         $this->pdo->exec(
             'CREATE TABLE IF NOT EXISTS monolog '
-            . '(channel VARCHAR(255), level INTEGER, message LONGTEXT, time INTEGER UNSIGNED)'
+            . '(channel VARCHAR(255), level INTEGER, message LONGTEXT, time INTEGER UNSIGNED, remoteip VARCHAR(255))'
         );
         $this->statement = $this->pdo->prepare(
-            'INSERT INTO monolog (channel, level, message, time) VALUES (:channel, :level, :message, :time)'
+            'INSERT INTO monolog (channel, level, message, time, remoteip) VALUES (:channel, :level, :message, :time, :remoteip)'
         );
 
         $this->initialized = true;
